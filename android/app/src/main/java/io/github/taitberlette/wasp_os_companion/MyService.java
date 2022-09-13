dbe683f56d67052d7cdda7ed1df7706cafc4668e
eacelmar96 <113272570+eacelmar96@users.noreply.github.com>
2022-09-13 21:46:56 +0200

    Update MyService.java

--- android/app/src/main/java/io/github/taitberlette/wasp_os_companion/MyService.java
+++ android/app/src/main/java/io/github/taitberlette/wasp_os_companion/MyService.java
@@ -979,11 +979,5 @@ public class MyService extends IntentService {
             }
         }
     }
-    
-    while(connectionState != 1){
-    Thread.sleep(15000);
-    scanForWatch(); //xk
-    }
-
 }
 
